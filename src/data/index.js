aac40ce7ae8c532ac24661e740262a2edcc8612a
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-12-21 10:33:47 +0300

    abstracted skills

---
+++ src/data/index.js
@@ -0,0 +1,13 @@
+export const skills = [
+  "React",
+  "Nextjs",
+  "Nodejs",
+  "Expressjs",
+  "Figma",
+  "MongoDB",
+  "Mongoose",
+  "TailwindCSS",
+  "Redux Toolkit",
+  "TypeScript",
+  "Git & Github",
+];
