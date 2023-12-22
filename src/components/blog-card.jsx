20d7978328d56f29d18ca0ee2a6b5fb96b8ac37e
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-12-22 17:09:55 +0300

    updated a and Link tags

--- src/components/blog-card.jsx
+++ src/components/blog-card.jsx
@@ -1,32 +1,32 @@
-import Link from "next/link";
+import { usePathname } from "next/navigation";
 import Image from "next/image";
 import logo from "@/assets/images/logo.svg";
 
-export default function BlogCard() {
+export default function BlogCard({ blog }) {
+  const pathname = usePathname();
+
   return (
-    <div className="bg-blue bg-opacity-20 p-6 rounded-xl">
+    <div
+      className={`bg-blue ${
+        pathname === "/" ? "mb-10" : "mb-0"
+      } bg-opacity-20 p-6 rounded-xl`}
+    >
       <span className="mb-6 block text-blue text-base">
-        {new Date().toLocaleDateString("en-US", {
+        {new Date(blog.publishedAt).toLocaleDateString("en-US", {
           year: "numeric",
           month: "long",
           day: "numeric",
         })}
-        {"  "}. 4 min read
       </span>
       <h2 className="text-3xl font-bold text-white mb-4 hover:text-blue transition-all duration-150">
-        Writing better and clean TailwindCSS code in 2023
+        {blog.title}
       </h2>
-      <p className="text-darkWhite text-base mb-6">
-        How can we write better and cleaner TailwindCSS code that is scalable
-        and and free from errors
-      </p>
+      <p className="text-darkWhite text-base mb-6">{blog.description}</p>
       <div className="flex items-center gap-x-4 text-white mb-6">
         <Image src={logo} alt="logo" />
         <span className="text-base text-darkWhite">Jacob Kyalo</span>
       </div>
-      <Link href="/blog/writing-better-and-clean-tailwindcss-code">
-        <span className="text-blue text-lg font-bold">Read more &rarr;</span>
-      </Link>
+      <span className="text-blue text-lg font-bold">Read more &rarr;</span>
     </div>
   );
 }
