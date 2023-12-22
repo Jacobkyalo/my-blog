14f389edc469b7119d61cae1dc4ab80214f63465
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-12-22 17:07:19 +0300

    blog page and dynamic blog pages

---
+++ src/pages/blog/[slug].jsx
@@ -0,0 +1,62 @@
+import Image from "next/image";
+import { useMDXComponent } from "next-contentlayer/hooks";
+import { allBlogs } from "contentlayer/generated";
+import Container from "@/components/container";
+import Hr from "@/components/hr";
+import { components } from "@/components/mdx-components";
+import logo from "@/assets/images/logo.svg";
+import Footer from "@/components/footer";
+
+export default function BlogPost({ blog }) {
+  const MDXContent = useMDXComponent(blog.body.code);
+
+  return (
+    <>
+      <main className="w-full md:container md:max-w-4xl">
+        <Container>
+          <section className="my-20 ">
+            <h1 className="mb-10 font-bold text-4xl sm:text-6xl text-white w-full max-w-6xl">
+              {blog.title}
+            </h1>
+            <span className="mb-6 block text-blue text-base">
+              Published on{" "}
+              {new Date(blog.publishedAt).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })}
+            </span>
+            <div className="flex items-center gap-x-4 text-white mb-10">
+              <Image src={logo} alt="logo" />
+              <span className="text-lg text-white">Jacob Kyalo</span>
+            </div>
+          </section>
+          <section className="prose text-darkWhite">
+            <MDXContent components={components} />
+          </section>
+        </Container>
+      </main>
+      <Container>
+        <Footer />
+      </Container>
+    </>
+  );
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: allBlogs.map((blog) => ({ params: { slug: blog.slug } })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const blog = allBlogs.find((blog) => blog.slug === params.slug);
+
+  if (!blog)
+    return {
+      notFound: true,
+    };
+
+  return { props: { blog } };
+}
---
+++ src/pages/blog/index.jsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import { allBlogs } from "contentlayer/generated";
+import { BsSearch } from "react-icons/bs";
+import BlogCard from "@/components/blog-card";
+import Container from "@/components/container";
+import Footer from "@/components/footer";
+
+export default function Blog() {
+  return (
+    <main>
+      <Container>
+        <section className="my-20">
+          <h1 className="mb-4 font-bold text-4xl sm:text-6xl text-white">
+            Latest blogs
+          </h1>
+          <p className="text-darkWhite text-lg mb-6 w-full max-w-lg">
+            Find some of my latest blogs here and read them to learn something
+            new today.
+          </p>
+          <form>
+            <div className="flex items-center justify-between p-3 border-2 border-blue rounded-md w-full max-w-2xl">
+              <input
+                type="text"
+                name="search"
+                placeholder="Search blogs..."
+                autoComplete="off"
+                className="bg-transparent outline-none text-white text-lg w-full px-2"
+              />
+              <BsSearch size={20} className="text-white font-bold" />
+            </div>
+          </form>
+        </section>
+        <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-16 mb-40">
+          {allBlogs
+            .sort(
+              (a, b) =>
+                new Date(b.publishedAt).getTime() -
+                new Date(a.publishedAt).getTime()
+            )
+            .map((blog) => (
+              <Link href={`/blog/${blog.slug}`} key={blog._id}>
+                <BlogCard blog={blog} />
+              </Link>
+            ))}
+        </section>
+        <Footer />
+      </Container>
+    </main>
+  );
+}
