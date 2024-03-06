cbcfd424f621a720696e1acaa59cd9894325ab19
Jacob Kyalo <jacobkyalo43@gmail.com>
2024-03-06 18:02:17 +0300

    Updated styles for paragraph and code elements

--- src/components/mdx-components.jsx
+++ src/components/mdx-components.jsx
@@ -7,7 +7,7 @@ export const components = {
   ),
   h3: (props) => <h1 className="text-xl font-semibold my-6" {...props} />,
   h4: (props) => <h1 className="text-lg font-semibold my-6" {...props} />,
-  p: (props) => <p className="text-base my-6" {...props} />,
+  p: (props) => <p className="text-base text-slate-600 my-6" {...props} />,
   ul: (props) => <ul className="list-disc ml-6 mb-6" {...props} />,
   ol: (props) => <ol className="list-decimal ml-6 mb-6" {...props} />,
   li: (props) => <li className="mb-2" {...props} />,
@@ -24,12 +24,12 @@ export const components = {
   ),
   pre: (props) => (
     <pre
-      className="bg-blue overflow-auto text-xs sm:text-sm w-full bg-opacity-20 rounded-xl p-4 my-8"
+      className="bg-black/90 text-white/90 overflow-auto text-xs sm:text-sm w-full bg-opacity-20 rounded-xl p-4 my-8"
       {...props}
     />
   ),
   code: (props) => (
-    <code className="text-darkWhite w-full rounded-sm p-1" {...props} />
+    <code className="text-white w-full rounded-sm p-1" {...props} />
   ),
 
   table: (props) => (
