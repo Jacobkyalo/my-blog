6e401b145adc8d1a005f63cd0b42791d93f64a58
Jacob Kyalo <jacobkyalo43@gmail.com>
2024-03-06 18:03:01 +0300

    Refactored ProjectCard component to use dynamic project data

--- src/components/project-card.jsx
+++ src/components/project-card.jsx
@@ -1,41 +1,19 @@
 import Link from "next/link";
-import Image from "next/image";
-import { BsGithub } from "react-icons/bs";
-import { MdLink } from "react-icons/md";
-import projectImage from "@/assets/images/dashboard.svg";
 
-export default function ProjectCard() {
+export default function ProjectCard({ project }) {
   return (
-    <div className="grid grid-cols-1 gap-20 md:grid-cols-2">
+    <div className="mb-8 border p-4 rounded-lg">
       <div>
-        <h2 className="text-4xl font-bold text-blue mb-8">Better Tailwind</h2>
-        <ul className="flex flex-wrap items-center gap-x-12 text-darkWhite">
-          <li>
-            <Link
-              href="#"
-              className="flex gap-x-4 items-center hover:text-blue transition-all duration-200 ease-in-out"
-            >
-              <MdLink size={30} className="text-blue" />
-              <span className="text-xl lg:text-2xl">Link</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="https://github.com/Jacobkyalo"
-              target="_blank"
-              className="flex gap-x-4 items-center hover:text-blue transition-all duration-200 ease-in-out"
-            >
-              <BsGithub size={24} className="text-blue" />
-              <span className="text-xl lg:text-xl">Github</span>
-            </Link>
-          </li>
-        </ul>
-        <p className="text-darkWhite text-lg lg:text-2xl my-10">
-          Better Tailwind is an award-winning component library that provides
-          the building blocks needed to create an accessible app with speed ⚡️.
-        </p>
+        <Link href={`${project?.href}`} target="_blank">
+          <h2 className="text-2xl font-bold mb-1">{project?.title}</h2>
+        </Link>
+        <p className="text-slate-600">{project?.description}</p>
+        <Link href={`${project?.href}`} target="_blank">
+          <span className="text-sm block mt-2 text-blue-600 font-bold mb-1">
+            View &rarr;
+          </span>
+        </Link>
       </div>
-      <Image src={projectImage} alt="project-image" priority />
     </div>
   );
 }
