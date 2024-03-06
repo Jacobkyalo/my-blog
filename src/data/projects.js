699f9598e883c31ace130ff5657ceb8d1a475c66
Jacob Kyalo <jacobkyalo43@gmail.com>
2024-03-06 18:04:18 +0300

    Added projects data

---
+++ src/data/projects.js
@@ -0,0 +1,12 @@
+export const projects = [
+  {
+    title: "Pakukaa",
+    description: "House rental management system  for house owners and tenants",
+    href: "http://pakukaa.vercel.app",
+  },
+  {
+    title: "Chatroom",
+    description: "Realtime chat application for Innovation Club members",
+    href: "http://ic-chatroom.vercel.app",
+  },
+];
