669cdd262c43ce2dd1f4b405be534fdca6f75169
Jacob Kyalo <jacobkyalo43@gmail.com>
2024-03-06 18:01:11 +0300

    Refactored BlogCard component

--- src/components/blog-card.jsx
+++ src/components/blog-card.jsx
@@ -1,32 +1,16 @@
-import { usePathname } from "next/navigation";
-import Image from "next/image";
-import logo from "@/assets/images/logo.svg";
+import Link from "next/link";
+import { format, parseISO } from "date-fns";
 
 export default function BlogCard({ blog }) {
-  const pathname = usePathname();
-
   return (
-    <div
-      className={`bg-blue ${
-        pathname === "/" ? "mb-10" : "mb-0"
-      } bg-opacity-20 p-6 rounded-xl`}
-    >
-      <span className="mb-6 block text-blue text-base">
-        {new Date(blog.publishedAt).toLocaleDateString("en-US", {
-          year: "numeric",
-          month: "long",
-          day: "numeric",
-        })}
-      </span>
-      <h2 className="text-3xl font-bold text-white mb-4 hover:text-blue transition-all duration-150">
-        {blog.title}
-      </h2>
-      <p className="text-darkWhite text-base mb-6">{blog.description}</p>
-      <div className="flex items-center gap-x-4 text-white mb-6">
-        <Image src={logo} alt="logo" />
-        <span className="text-base text-darkWhite">Jacob Kyalo</span>
-      </div>
-      <span className="text-blue text-lg font-bold">Read more &rarr;</span>
+    <div className="my-8 border-b py-4 rounded-lg">
+      <time dateTime={blog?.publishedAt} className="italic text-slate-600">
+        {format(parseISO(blog?.publishedAt), "LLLL d, yyyy")}
+      </time>
+      <Link href={`/blog/${blog?.slug}`}>
+        <h2 className="text-2xl font-bold">{blog?.title}</h2>
+      </Link>
+      <p className="text-slate-600">{blog?.description}</p>
     </div>
   );
 }
