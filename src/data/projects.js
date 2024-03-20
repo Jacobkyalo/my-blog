beee7e8907b132916889c46c5be5b46d713ad4b7
Jacob Kyalo <jacobkyalo43@gmail.com>
2024-03-20 17:52:02 +0300

    new: add new project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,12 +1,17 @@
 export const projects = [
   {
+    title: "Taskr",
+    description: "Task management application for your tasks",
+    href: "https://my-taskr.vercel.app",
+  },
+  {
     title: "Pakukaa",
     description: "House rental management system  for house owners and tenants",
-    href: "http://pakukaa.vercel.app",
+    href: "https://pakukaa.vercel.app",
   },
   {
     title: "Chatroom",
     description: "Realtime chat application for Innovation Club members",
-    href: "http://ic-chatroom.vercel.app",
+    href: "https://ic-chatroom.vercel.app",
   },
 ];
