9359510c04f1b0d88b47d7cf6c81cab6386faabc
Jacob Kyalo <jacobkyalo43@gmail.com>
2024-03-24 11:02:04 +0300

    render logo

--- src/components/app-navbar.jsx
+++ src/components/app-navbar.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 const AppNavbarLinks = [
@@ -13,7 +14,13 @@ export default function AppNavbar() {
 
   return (
     <header className="flex flex-col sm:flex-row justify-center sm:justify-between items-center gap-6 sm:gap-0 py-8">
-      <Link href="/">
+      <Link href="/" className="flex items-center gap-x-4">
+        <Image
+          src="/logo_path.svg"
+          alt="app_logo_path"
+          width={30}
+          height={30}
+        />
         <h2 className="font-bold text-black text-lg">Jacob</h2>
       </Link>
       <nav className="bg-white bg-opacity-0 rounded-full shadow-lg sm:shadow-none p-4 sm:p-0 border sm:border-none">
